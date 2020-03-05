2c90bac3d0fe3f2b28ec4c964dc09acbbc1847cd
knesmeyanov <k.nesmeyanov@rambler-co.ru>
2020-03-05 17:55:50 +0300

    Fix branch

--- ProvidesOffset.php
+++ ProvidesOffset.php
@@ -1,33 +1,23 @@
 <?php
-
 /**
- * This file is part of phplrt package.
+ * This file is part of Phplrt package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 declare(strict_types=1);
 
 namespace Phplrt\Position;
 
 /**
- * Interface PositionInterface
+ * Providing the ability to get an offset in bytes from the source text.
  */
-interface PositionInterface
+interface ProvidesOffset
 {
     /**
+     * Returns an offset value in bytes.
+     *
      * @return int
      */
     public function getOffset(): int;
-
-    /**
-     * @return int
-     */
-    public function getLine(): int;
-
-    /**
-     * @return int
-     */
-    public function getColumn(): int;
 }
